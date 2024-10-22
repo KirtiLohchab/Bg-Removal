9c74d119011de3294b8d5cb242076bdb6e1ff731
Kirti Lohchab <ahlawatkirti227@gmail.com>
2024-10-22 14:05:08 +0530

    manage password

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,26 +1,26 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   mongoose.connection.on("connected", () => {
-//     console.log("DB connected");
-//   });
-//   await mongoose.connect(`${process.env.MONGODB_URL}/bg-removal`);
-// };
-// export default connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/bg-removal`
-    );
-    console.log(
-      `DB connected !! DB HOST: ${connectionInstance.connection.host}`
-    );
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+  mongoose.connection.on("connected", () => {
+    console.log("DB connected");
+  });
+  await mongoose.connect(`${process.env.MONGODB_URL}/bg-removal`);
 };
 export default connectDB;
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const connectionInstance = await mongoose.connect(
+//       `${process.env.MONGODB_URL}/bg-removal`
+//     );
+//     console.log(
+//       `DB connected !! DB HOST: ${connectionInstance.connection.host}`
+//     );
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+// export default connectDB;
